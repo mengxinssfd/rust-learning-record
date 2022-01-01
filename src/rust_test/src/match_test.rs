2f1b59675b5056cb424ad8e9428a6f597b027ff7
dyh_a <xinzon32177@163.com>
2022-01-02 03:39:15 +0800

    match test

---
+++ src/rust_test/src/match_test.rs
@@ -0,0 +1,48 @@
+pub fn match_test() {
+    // let some_u8_value = 0u8;
+    let some_u8_value = 5u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        // _ 通配符
+        _ => (),
+    }
+
+    let c = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("{}", c);
+
+    // if let
+    let coin = Coin::Penny;
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+    println!("count {}", count);
+}
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
