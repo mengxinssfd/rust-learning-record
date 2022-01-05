d57a5d9ff81211a442688dce35fbce3eed19d3a1
DYH <xinzon32177@163.com>
2022-01-05 18:10:43 +0800

    minigrep 分离文件 test未完成

--- src/minigrep/src/lib.rs
+++ src/minigrep/src/lib.rs
@@ -23,3 +23,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("text {}", contents);
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
+// https://kaisery.github.io/trpl-zh-cn/ch12-04-testing-the-librarys-functionality.html
