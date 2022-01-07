9b882b91a20e742c565a2f1a45744113c0e37594
DYH <xinzon32177@163.com>
2022-01-07 18:46:18 +0800

    minigrep 分离文件 区分大小写

--- src/minigrep/src/lib.rs
+++ src/minigrep/src/lib.rs
@@ -1,9 +1,13 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    // windows 命令行输入
+    // $env:CASE_INSENSITIVE='' 关闭
+    // $env:CASE_INSENSITIVE=1 开启
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -13,7 +17,8 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
@@ -21,11 +26,36 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     println!("text {}", contents);
+    let results = if config.case_sensitive {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in results {
+        println!("result: {}", line);
+    }
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let low_query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&low_query) {
+            results.push(line)
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -45,5 +75,36 @@ Pick three.";
             search(query, contents)
         );
     }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
 // https://kaisery.github.io/trpl-zh-cn/ch12-04-testing-the-librarys-functionality.html
+
