e3d99f7334c63cb15564303256f5d3a8777e5b61
DYH <xinzon32177@163.com>
2022-01-11 18:06:03 +0800

    rust_test box_test

--- src/rust_test/src/box_test.rs
+++ src/rust_test/src/box_test.rs
@@ -69,7 +69,7 @@ pub fn main() {
 
     // 解引用强制多态
     fn hello(str: &str) {
-        println!("hello {}",str);
+        println!("hello {}", str);
     }
     let s = String::from("test");
     hello(&s);
@@ -109,4 +109,40 @@ pub fn main() {
         let d = CustomSmartPointer { data: String::from("自动drop") }; // 后
     }
 
+
+    // 共用一个智能指针
+    /* let a = Cons(5,
+                 Box::new(Cons(10,
+                               Box::new(Nil))));
+    let b = Cons(3, Box::new(a));
+    let c = Cons(4, Box::new(a)); // error  */
+    // 引用计数智能指针
+    use_rc();
+}
+
+// 引用计数智能指针
+fn use_rc() {
+    enum List {
+        Cons(i32, Rc<List>),
+        Nil,
+    }
+
+    use List::{Cons, Nil};
+    use std::rc::Rc;
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
+
+
+    // print
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
