dc33cd065ead8fb4e3255ef659db2147236bfb49
DYH <xinzon32177@163.com>
2022-01-15 12:02:34 +0800

    rust_test oo_test

--- src/rust_test/src/oo_test.rs
+++ src/rust_test/src/oo_test.rs
@@ -1,7 +1,136 @@
 #![allow(unused)]
+// oo: object-oriented面向对象
 
-pub fn main(){}
+pub fn main() {
+    demo_2();
+    demo_3();
+}
 
-fn demo_1(){
+fn demo_1() {
+    pub trait Draw {
+        fn draw(&self);
+    }
+    pub struct Screen {
+        pub components: Vec<Box<dyn Draw>>,
+    }
+    impl Screen {
+        pub fn run(&self) {
+            for component in self.components.iter() {
+                component.draw();
+            }
+        }
+    }
+    pub struct Button {
+        pub width: u32,
+        pub height: u32,
+        pub label: String,
+    }
+    impl Draw for Button {
+        fn draw(&self) {
+            // 实际绘制按钮的代码
+        }
+    }
+}
 
+fn demo_2() {
+    trait State {
+        fn content<'a>(&self, post: &'a Post) -> &'a str {
+            ""
+        }
+        fn request_review(self: Box<Self>) -> Box<dyn State>;
+        fn approve(self: Box<Self>) -> Box<dyn State>;
+    }
+    struct Draft {}
+    impl State for Draft {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            Box::new(PendingReview {})
+        }
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+    }
+    pub struct Post {
+        state: Option<Box<dyn State>>,
+        content: String,
+    }
+    impl Post {
+        pub fn new() -> Post {
+            Post {
+                state: Some(Box::new(Draft {})),
+                content: String::new(),
+            }
+        }
+        pub fn add_text(&mut self, text: &str) {
+            self.content.push_str(text);
+        }
+        pub fn content(&self) -> &str {
+            self.state.as_ref().unwrap().content(self)
+        }
+        pub fn request_review(&mut self) {
+            if let Some(s) = self.state.take() {
+                self.state = Some(s.request_review())
+            }
+        }
+    }
+
+    struct PendingReview {}
+    impl State for PendingReview {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            Box::new(Published {})
+        }
+    }
+
+    struct Published {}
+    impl State for Published {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+        fn content<'a>(&self, post: &'a Post) -> &'a str {
+            &post.content
+        }
+    }
+
+    let mut post = Post::new();
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+}
+
+// TODO 未完
+fn demo_3() {
+    pub struct Post {
+        content: String,
+    }
+
+    pub struct DraftPost {
+        content: String,
+    }
+
+    impl Post {
+        pub fn new() -> DraftPost {
+            DraftPost {
+                content: String::new(),
+            }
+        }
+
+        pub fn content(&self) -> &str {
+            &self.content
+        }
+    }
+
+    impl DraftPost {
+        pub fn add_text(&mut self, text: &str) {
+            self.content.push_str(text);
+        }
+        pub fn request_review(self) -> PendingReviewPost {
+            PendingReviewPost {
+                content: self.content,
+            }
+        }
+    }
 }
