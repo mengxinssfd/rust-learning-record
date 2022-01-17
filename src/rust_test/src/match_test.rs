3d78c06b036eddf34f0b20f64ec056b45026f8b5
dyh_a <xinzon32177@163.com>
2022-01-18 00:42:40 +0800

    rust_test match_test 未完 解构结构体和元组

--- src/rust_test/src/match_test.rs
+++ src/rust_test/src/match_test.rs
@@ -218,6 +218,45 @@ fn demo_11() {
     }
 }
 
+// 解构嵌套的枚举
+fn demo_12() {
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(Color),
+    }
+
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!(
+                "Change the color to hue {}, saturation {}, and value {}",
+                h, s, v
+            )
+        }
+        _ => {}
+    }
+}
+
+// 解构结构体和元组
+fn demo_13() {
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+}
+
 pub fn main() {
     demo_1();
     demo_2();
@@ -229,6 +268,9 @@ pub fn main() {
     demo_8();
     demo_9();
     demo_10();
+    demo_11();
+    demo_12();
+    demo_13();
 }
 
 #[derive(Debug)]
