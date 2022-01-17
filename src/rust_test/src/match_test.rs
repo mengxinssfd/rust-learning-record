717f0b667120d3ea3f610a4190c782c4d3f4c233
DYH <xinzon32177@163.com>
2022-01-17 12:58:54 +0800

    rust_test match_test 未完

--- src/rust_test/src/match_test.rs
+++ src/rust_test/src/match_test.rs
@@ -110,15 +110,89 @@ fn demo_6() {
     let point = (3, 5);
     print_coordinates(&point);
 }
+
 // 多个模式，匹配多个值
 fn demo_7() {
     let x = 1;
     match x {
-        1|2=>println!("1|2: {}",x),
-        3 => println!("3: {}",x),
+        1 | 2 => println!("1|2: {}", x),
+        3 => println!("3: {}", x),
         _ => {}
     }
 }
+
+// 匹配命名变量
+fn demo_8() {
+    let x = Some(5);
+    let y = 10;
+    match x {
+        // 只匹配50
+        Some(50) => println!("Got 50"),
+        // 匹配除了50和None的任何值
+        Some(y) => println!("Matched, y = {:?}", y), // 内部变量y，与外部y无关
+        // 匹配以上匹配值以外的任何值，包括None
+        _ => println!("Default case, x = {:?}", x)
+    }
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+}
+
+// 通过 ..= 匹配值的范围  语法允许你匹配一个闭区间范围内的值
+fn demo_9() {
+    // 范围只允许用于数字或 char 值
+    fn mc(x: i32) {
+        match x {
+            // 相当于 1 <= x <= 5
+            // 相当于rust的 1|2|3|4|5
+            1..=5 => println!("one through five {}", x),
+            _ => println!("something else {}", x)
+        }
+    }
+    fn mc_char(x: char) {
+        match x {
+            'a'..='j' => println!("early ASCII letter"),
+            'k'..='z' => println!("late ASCII letter"),
+            _ => println!("something else")
+        }
+    }
+    mc(5);
+    mc(1);
+    mc(3);
+    mc(0);
+    mc(6);
+
+    // early ASCII letter
+    mc_char('a');
+    mc_char('j');
+    // late ASCII letter
+    mc_char('k');
+    mc_char('z');
+    // something else
+    mc_char('Z');
+    mc_char('B');
+}
+
+// 解构结构体
+fn demo_10() {
+    println!("-match_test-demo_10-");
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    let p = Point { x: 0, y: 7 };
+    let Point { x, y } = p;
+    println!("x:{},y:{}", x, y);
+    let Point { x, .. } = p;
+    println!("x:{}", x);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({},{})", x, y),
+    }
+
+    println!("-match_test-demo_10-");
+}
+
 pub fn main() {
     demo_1();
     demo_2();
@@ -127,6 +201,9 @@ pub fn main() {
     demo_5();
     demo_6();
     demo_7();
+    demo_8();
+    demo_9();
+    demo_10();
 }
 
 #[derive(Debug)]
@@ -134,12 +211,14 @@ enum UsState {
     Alabama,
     Alaska,
 }
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
     Quarter(UsState),
 }
+
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
         Coin::Penny => 1,
