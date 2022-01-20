8779b923a95c97152a8b1156fc15b2ac7b395c1f
DYH <xinzon32177@163.com>
2022-01-20 18:00:28 +0800

    rust_test advanced_traits_test.rs 高级trait 完

--- src/rust_test/src/advanced_traits_test.rs
+++ src/rust_test/src/advanced_traits_test.rs
@@ -77,7 +77,79 @@ fn demo_2() {
     Wizard::fly(&h); // fly from Wizard
 }
 
+/// 完全限定语法与消歧义：调用相同名称的方法（无self）
+fn demo_3() {
+    trait Animal {
+        fn baby_name() -> String;
+    }
+    struct Dog;
+
+    impl Dog {
+        fn baby_name() -> String {
+            String::from("puppy")
+        }
+    }
+    impl Animal for Dog {
+        fn baby_name() -> String {
+            String::from("Spot")
+        }
+    }
+
+    println!("A baby dog is called a {}", Dog::baby_name()); // puppy
+    // <Type as Trait>::function(receiver_if_method, next_arg, ...);
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name()); // Spot
+}
+
+use std::fmt;
+use std::fmt::Formatter;
+use std::path::Display;
+
+/// 在另一个 trait 中使用某 trait 的功能
+fn demo_4() {
+    trait OutlinePrint: fmt::Display {
+        fn outline_print(&self) {
+            let output = self.to_string();
+            let len = output.len();
+            println!("{}", "*".repeat(len + 4));
+            println!("*{}*", " ".repeat(len + 2));
+            println!("* {} *", output);
+            println!("*{}*", " ".repeat(len + 2));
+            println!("{}", "*".repeat(len + 4));
+        }
+    }
+
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    impl fmt::Display for Point {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "({},{})", self.x, self.y)
+        }
+    }
+    impl OutlinePrint for Point {}
+
+    let p = Point { x: 10, y: 20 };
+
+    p.outline_print();
+}
+
+/// newtype 模式用以在外部类型上实现外部 trait
+fn demo_5() {
+    struct Wrapper(Vec<String>);
+    impl fmt::Display for Wrapper {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "[{}]", self.0.join(","))
+        }
+    }
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("Wrapper {}", w);
+}
+
 pub fn main() {
     demo_1();
     demo_2();
+    demo_3();
+    demo_4();
+    demo_5();
 }
