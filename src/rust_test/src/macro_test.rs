744cff3146f8c0348ec63fc0ca7fe9b9bc1053c9
DYH <xinzon32177@163.com>
2022-01-22 18:22:35 +0800

    rust_test macro_test.rs

--- src/rust_test/src/macro_test.rs
+++ src/rust_test/src/macro_test.rs
@@ -3,10 +3,12 @@
 fn demo_1() {
     #[macro_export]
     macro_rules! vec2 {
+        // "*"类似于正则的"*"
         ( $( $x:expr ),* ) => {
             {
                 let mut temp_vec = Vec::new();
                 $(
+                    println!("x,{}", $x);
                     temp_vec.push($x);
                 )*
                 temp_vec
