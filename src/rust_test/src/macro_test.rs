00311cbe1ae4cb8a4bba050aa376bf7a17244c9d
DYH <xinzon32177@163.com>
2022-01-24 13:01:08 +0800

    rust_test macro_test.rs 自定义过程宏

--- src/rust_test/src/macro_test.rs
+++ src/rust_test/src/macro_test.rs
@@ -23,8 +23,21 @@ fn demo_1() {
     println!("v,{:?}", v);
 }
 
+// 自定义过程宏
+fn demo_2() {
+    use hello_macro::HelloMacro;
+    use hello_macro_derive::HelloMacro;
+
+    #[derive(HelloMacro)]
+    struct HelloMacroTest;
+
+    HelloMacroTest::hello_macro();
+}
+
 pub fn main() {
     demo_1();
     // let v = vec2![1, 2, 3];
     // println!("v,{:?}", v);
+
+    demo_2();
 }
