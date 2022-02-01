b4802e51bb0721341171eaad16b3dc503cf53c36
dyh_a <xinzon32177@163.com>
2022-02-01 14:00:21 +0800

    rust_test macro_test.rs 完

--- src/rust_test/src/macro_test.rs
+++ src/rust_test/src/macro_test.rs
@@ -34,10 +34,38 @@ fn demo_2() {
     HelloMacroTest::hello_macro();
 }
 
+// 群里写的
+fn demo_3() {
+    macro_rules! my {
+        ($($id:ident)/+) => {
+            vec![1]
+        };
+        ($id:ident $(+ $id2:ident)+) => {
+            vec![2]
+        };
+        ($id:ident $(/ $id2:ident)+; $($tail:tt)+) => {
+            {let mut x = vec![1];x.append(&mut my!($($tail)+));x}
+        };
+        ($id:ident $(+ $id2:ident)+; $($tail:tt)+) => {
+            {let mut x = vec![2];x.append(&mut my!($($tail)+));x}
+        };
+    }
+
+    println!("{:?}", my!(a / b));
+    println!("{:?}", my!(a / b / c));
+    println!("{:?}", my!(a + b));
+    println!("{:?}", my!(a + b + c));
+    println!("{:?}", my!(a/b;a+b+c));
+    println!("{:?}", my!(a+b;a/b/c));
+    println!("{:?}", my!(a+b;a/b/c;c+d));
+    println!("{:?}", my!(a+b;a/b/c;c+d;c/d));
+}
+
 pub fn main() {
     demo_1();
     // let v = vec2![1, 2, 3];
     // println!("v,{:?}", v);
 
     demo_2();
+    demo_3();
 }
