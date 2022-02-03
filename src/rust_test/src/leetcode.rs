3f46997df70a01d89d96968f5ce92c89804fae5e
dyh_a <xinzon32177@163.com>
2022-02-03 14:15:25 +0800

    rust_test leetcode.rs add_binary majority_element v2

--- src/rust_test/src/leetcode.rs
+++ src/rust_test/src/leetcode.rs
@@ -164,16 +164,11 @@ fn tree() {
 
         pub fn majority_element(nums: Vec<i32>) -> i32 {
             let mut map = HashMap::new();
-            nums.iter().for_each(|n| {
-                map.insert(n, map.get(n).unwrap_or(&0) + 1);
+            nums.iter().for_each(|&n| {
+                map.insert(n, map.get(&n).unwrap_or(&0) + 1);
             });
             let half = nums.len() / 2;
-            for (k, &v) in map.iter() {
-                if v > half {
-                    return **k;
-                }
-            }
-            0
+            *map.iter().find(|(_, &v)| v > half).unwrap().0 as i32
         }
     }
 }
