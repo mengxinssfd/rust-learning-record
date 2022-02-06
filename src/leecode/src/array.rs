67990555d194591f3a055a422d5eddc9d3480ef2
dyh_a <xinzon32177@163.com>
2022-02-07 00:36:24 +0800

    leetcode 杨辉三角

--- src/leecode/src/array.rs
+++ src/leecode/src/array.rs
@@ -10,11 +10,47 @@ impl Solution {
         let half = nums.len() / 2;
         *map.iter().find(|(_, &v)| v > half).unwrap().0 as i32
     }
+
+    /// 杨辉三角  // https://leetcode-cn.com/problems/pascals-triangle/
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let mut res = vec![vec![1]];
+        for i in 2..=num_rows as usize {
+            // let last_row = res.get(i as usize - 2).unwrap();
+            let last_row = res.last().unwrap();
+            let mut row = vec![1];
+            for j in 1..i - 1 {
+                row.push(*last_row.get(j - 1).unwrap() + last_row.get(j).unwrap());
+            }
+            row.push(1);
+            res.push(row);
+        }
+        res
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::Solution;
+
+    #[test]
+    fn generate() {
+        for i in 1..6 {
+            println!("1,{}", i);
+        }
+        for i in 1..=6 {
+            println!("2,{}", i);
+        }
+        assert_eq!(
+            Solution::generate(5),
+            vec![
+                vec![1],
+                vec![1, 1],
+                vec![1, 2, 1],
+                vec![1, 3, 3, 1],
+                vec![1, 4, 6, 4, 1]
+            ]
+        );
+    }
 
     #[test]
     fn majority_element() {
