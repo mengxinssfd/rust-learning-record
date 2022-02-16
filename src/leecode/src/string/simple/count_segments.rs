5a1f067bc62cb2d8308913e5650395be06cede53
DYH <xinzon32177@163.com>
2022-02-16 18:00:41 +0800

    leetcode 字符串中的单词数

--- src/leecode/src/string/simple/count_segments.rs
+++ src/leecode/src/string/simple/count_segments.rs
@@ -2,6 +2,7 @@ use crate::solution::Solution;
 
 /// 434. 字符串中的单词数 // https://leetcode-cn.com/problems/number-of-segments-in-a-string/
 impl Solution {
+    // 转为char
     pub fn count_segments(s: String) -> i32 {
         let mut count = 0;
         let mut cs = s.chars();
@@ -16,6 +17,19 @@ impl Solution {
 
         count
     }
+    // 转为bytes
+    pub fn count_segments2(s: String) -> i32 {
+        let mut count = 0;
+        let s = s.as_bytes();
+
+        for i in 0..s.len() {
+            if (i == 0 || s[i - 1] == b' ') && s[i] != b' ' {
+                count += 1;
+            }
+        }
+
+        count
+    }
 }
 
 #[cfg(test)]
@@ -23,7 +37,13 @@ mod test {
     use super::Solution;
 
     #[test]
-    fn majority_element() {
+    fn test_byte() {
+        assert_eq!(b'a', 97);
+    }
+
+    #[test]
+    fn count_segments() {
         assert_eq!(Solution::count_segments("Hello, my name is John".to_string()), 5);
+        assert_eq!(Solution::count_segments2("Hello, my name is John".to_string()), 5);
     }
 }
